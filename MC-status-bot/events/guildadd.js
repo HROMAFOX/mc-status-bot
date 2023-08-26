34d848ab56bac6d8a5bffcbb9edb812e884a0adb
HROMAFOX <113515660+HROMAFOX@users.noreply.github.com>
2023-08-26 13:35:13 +0300

    Add files via upload

---
+++ MC-status-bot/events/guildadd.js
@@ -0,0 +1,21 @@
+const Server = require('../database/ServerSchema')
+const logger = require('../modules/nodeLogger.js')
+const { createCache } = require('../modules/cache.js')
+
+module.exports = {
+  name: 'guildCreate',
+  execute(guild) {
+    logger.info(`Joined new guild: ${guild.name}`)
+
+    const server = new Server({
+      _id: guild.id
+    })
+    server
+      .save()
+      .then(() => {
+        logger.info('Added the server db entry.')
+        createCache('Server', guild.id)
+      })
+      .catch((err) => logger.error(err))
+  }
+}
